f4884b04e9acb8c7f748672534ad30398d3a4823
sunaliseth <sunaliseth01@gmail.com>
2017-09-13 14:59:40 +0100

    Post/redirect Get pattern

--- app.rb
+++ app.rb
@@ -1,17 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Application
-get '/' do
+  enable :sessions
+
+  get '/' do
   #"Testing infrastructure working"
   erb(:index)
-end
+  end
 
-post '/names' do
+  post '/names' do
   p params
-  @name1 = params[:name1]
-  @name2 = params[:name2]
-  erb(:play)
+  session[:name1] = params[:name1]
+  session[:name2] = params[:name2]
+  redirect to('/play')
 end
 
+  get'/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+  erb(:play)
+  end
 run! if app_file == $0 #ASK ED WHY DIDNT RUN WHEN WAS BEFORE '/'
 end
