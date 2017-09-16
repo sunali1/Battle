30a17da6a0b8e73abf861bb3544568ed1f222a7f
sunaliseth <sunaliseth01@gmail.com>
2017-09-16 13:58:26 +0100

    Skinny Controllers

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require "./lib/player"
+require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -9,24 +10,22 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-  p params
-  $player_1 = Player.new(params[:player_1_name])
-  $player_2 = Player.new(params[:player_2_name])
-  p $player_1 #in names
-  p $player_2
+  player_1 = Player.new(params[:player_1_name])
+  player_2 = Player.new(params[:player_2_name])
+  p player_1 #in names
+  p player_2
+  $game = Game.new(player_1, player_2)
   redirect '/play'
 end
 
   get'/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb(:play)
   end
 
   get'/attack' do
-    @player_1= $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb(:attack)
     end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
   let(:player_1) { double :player }
   let(:player_2) { double :player }
 
   describe "attack" do
     it "damages the player" do
       expect(player_2).to receive(:receive_damage)
-      game.attack(player_2)
+      game.attack(game.player_2)
     end
   end
+
+  describe "player_1" do
+    it "retreives first player" do
+      expect(player_1).to eq(player_1)
+    end
+  end
+  describe "player_2" do
+    it "retreives second player" do
+      expect(player_2).to eq(player_2)
+    end
+  end
+
 end
